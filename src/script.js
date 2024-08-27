cabdc35307b923a0fec99c4694490f54c1d75f7d
Seth Krantzler <sethkrantzler@gmail.com>
2024-08-27 12:06:49 -0400

    Fixing control snapping easing

--- src/script.js
+++ src/script.js
@@ -643,8 +643,8 @@ function resetControls() {
 
 //#region Debug
 const debugObject = {}
-debugObject.resetEase = "elastic.out(1, 2)"
-debugObject.easeDuration = 0.8
+debugObject.resetEase = "power1.out"
+debugObject.easeDuration = 0.25
 debugObject.spawnDistance = 16
 debugObject.fetchInterval = 6
 debugObject.posterSelectionHeight = 0.75
